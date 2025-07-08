bf71c8833554f615f896fe7c17f1834d0ee291c3
berkaykyb <berkayabasi2003@hotmail.com>
2025-07-08 17:13:46 +0300

    Initial commit of Hangman Game

--- Week7_PreLab/obj/Debug/net8.0-windows/Week7_PreLab.AssemblyInfo.cs
+++ Week7_PreLab/obj/Debug/net8.0-windows/Week7_PreLab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week7_PreLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ac850ec2c6ff8dad399abaaeece3341212aec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week7_PreLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week7_PreLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
